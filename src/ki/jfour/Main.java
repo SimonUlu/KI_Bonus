d2d14dce44461776589c1f346fd35e5e6a3b6bfd
Simon Unterlugauer <simon.unterlugauer@inno-brain.de>
2022-12-14 18:00:32 +0100

    Added different heuristics and get myPlayer

---
+++ src/ki/jfour/Main.java
@@ -0,0 +1,27 @@
+package ki.jfour;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Board b = new Board(7, 7);
+
+        while (b.possibleMoves().size() > 0 && b.getWinner() == Player.NONE) {
+
+            if (b.getWinner() != Player.NONE) {
+                b.getWinner();
+            }
+
+            Move gameMoveMinMax = new MyMinMaxAI().findBestMove(b);
+            b = b.executeMove(gameMoveMinMax);
+            System.out.println(b);
+
+            Move gameMoveMCT = new MyMCTSAI().findBestMonteMove(b, Player.BLUE);
+            b = b.executeMove(gameMoveMCT);
+            System.out.println(b);
+
+        }
+
+    }
+}
